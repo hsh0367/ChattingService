a19cfff932e8840f287dcc2a79af2e5381c6e70d
heosanghun <hsh0367@naver.com>
2019-01-21 20:52:09 +0900

    ChattingService : change User DB

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,39 +1,51 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "User")
 public class User {
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
-    private String name;
+	@Id
+	@Column(name = "User_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    protected User() {}
+	@Column(name = "User_name")
+	private String name;
 
-    public User(String name) {
-        this.name = name;
-    }
+	@OneToMany(mappedBy = "User")
+	private List<User> users = new ArrayList<User>();
 
-    public Long getId() {
-        return id;
-    }
+	protected User() {
+	}
 
-    public String getName() {
-        return name;
-    }
+	public User(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    @Override
-    public String toString() {
-        return String.format("User[id='%d', name='%s']", id, name);
-    }
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("User[id='%d', name='%s']", id, name);
+	}
 }
