4a88041ae462eba4f4789bfb6bde3de5fafcbc58
heosanghun <hsh0367@naver.com>
2019-01-21 20:52:50 +0900

    ChattingService : change Chat DB

--- src/main/java/com/example/demo/Chat.java
+++ src/main/java/com/example/demo/Chat.java
@@ -1,43 +1,56 @@
 package com.example.demo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Chat")
-public class Chat {//채팅 디비파일
-	
+public class Chat {// 채팅 디비파일
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "Chat_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name = "Chat_msg")
 	private String msgContext;
-	private Long userId;
+
+	@ManyToOne
+	@JoinColumn(name = "User_id")
+	private User user;
+
 	public Chat() {
 	}
+
 	public Chat(String msg) {
 		this.msgContext = msg;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getMsgContext() {
 		return msgContext;
 	}
+
 	public void setMsgContext(String msgContext) {
 		this.msgContext = msgContext;
 	}
-	
+
 	@Override
-	public String toString(){
-		return String.format("Chat[id='%d', msg='%s']", id,msgContext);
+	public String toString() {
+		return String.format("Chat[id='%d', msg='%s']", id, msgContext);
 	}
-		
+
 }
